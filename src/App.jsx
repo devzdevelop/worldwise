f36389d52e20398298abc205c7887e64cfca46dd
devzdevelop <devbusinessinfluencer@gmail.com>
2024-07-08 17:25:49 -0500

    third commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Product from './pages/Product';
 import Pricing from './pages/Pricing';
+import Login from './pages/Login';
 import AppLayout from './pages/AppLayout';
 import PageNotFound from './pages/PageNotFound';
 import Homepage from './pages/Homepage';
@@ -12,7 +13,12 @@ function App() {
         <Route path='/' element={<Homepage />} />
         <Route path='product' element={<Product />} />
         <Route path='pricing' element={<Pricing />} />
-        <Route path='app' element={<AppLayout />} />
+        <Route path='login' element={<Login />} />
+        <Route path='app' element={<AppLayout />}>
+          <Route path='cities' element={<p>List of cities</p>} />
+          <Route path='countries' element={<p>List of countries</p>} />
+          <Route path='form' element={<p>Form</p>} />
+        </Route>
         <Route path='*' element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
--- src/pages/AppLayout.jsx
+++ src/pages/AppLayout.jsx
@@ -1,10 +1,13 @@
 import AppNav from '../components/AppNav';
+import Map from '../components/Map';
+import Sidebar from '../components/Sidebar';
+import styles from './AppLayout.module.css';
 
 export default function AppLayout() {
   return (
-    <div>
-      <AppNav />
-      <div>App</div>
+    <div className={styles.app}>
+      <Sidebar />
+      <Map />
     </div>
   );
 }
