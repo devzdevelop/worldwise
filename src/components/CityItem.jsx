e23ead42241f353eb49837ec71b675b9743cf2d0
devzdevelop <devbusinessinfluencer@gmail.com>
2024-07-11 16:09:11 -0500

    Include leaflet library and display markers for added cities on the map.

--- src/components/CityItem.jsx
+++ src/components/CityItem.jsx
@@ -12,7 +12,7 @@ const formatDate = (date) =>
 export default function CityItem({ city }) {
   const { currentCity } = useCities();
   const { cityName, emoji, date, id, position } = city;
-  console.log(city);
+  // console.log(city);
   return (
     <li>
       <Link
--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,11 +1,23 @@
 import { useNavigate, useSearchParams } from 'react-router-dom';
 import styles from './Map.module.css';
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
+import { useEffect, useState } from 'react';
+import { useCities } from '../context/CitiesContext';
 export default function Map() {
   const navigate = useNavigate();
+  const { cities } = useCities();
+  const [mapPosition, setMapPosition] = useState([40, 0]);
 
   const [searchParams, setSearchParams] = useSearchParams();
   let lat = searchParams.get('lat');
   let lng = searchParams.get('lng');
+
+  console.log('searchParams: ', searchParams.get('lat'));
+  useEffect(() => {
+    lat && lng && setMapPosition([Number(lat), Number(lng)]);
+  }, [lat, lng]);
+
+  console.log('mapPosition: ', mapPosition);
 
   return (
     <div
@@ -14,18 +26,27 @@ export default function Map() {
         navigate('form');
       }}
     >
-      {lat && (
-        <p>
-          Position: {lat}, {lng}
-        </p>
-      )}
-      <button
-        onClick={() => {
-          setSearchParams({ lat: 23, lng: 50 });
-        }}
+      <MapContainer
+        center={mapPosition}
+        zoom={13}
+        scrollWheelZoom={true}
+        className={styles.map}
       >
-        Change position
-      </button>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url='https://{s}.tile.openstreetmap.fr/hot//{z}/{x}/{y}.png'
+        />
+        {cities.map((city) => (
+          <div key={city.id}>
+            <Marker position={[city.position.lat, city.position.lng]}>
+              <Popup>
+                <span>{city.emoji}</span>
+                <span>{city.cityName}</span>
+              </Popup>
+            </Marker>
+          </div>
+        ))}
+      </MapContainer>
     </div>
   );
 }
